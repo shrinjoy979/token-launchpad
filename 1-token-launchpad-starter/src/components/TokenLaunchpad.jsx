f670ec91d550d834ee80caa28cda2639296a1b7a
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2024-12-16 22:29:07 +0530

    launchpad form UI and connect wallet module

--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,5 +1,44 @@
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from "@solana/spl-token";
 
 export function TokenLaunchpad() {
+    const { connection } = useConnection();
+    const wallet = useWallet();
+
+    async function createToken() {
+        const mintKeypair = Keypair.generate();
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID, // Who own this accounts
+            }),
+            createInitializeMint2Instruction(mintKeypair.publicKey, decimals, mintAuthority, freezeAuthority, TOKEN_PROGRAM_ID)
+        );
+            
+        // transaction.feePayer = wallet.publicKey;
+        // transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        transaction.partialSign(mintKeypair);
+
+        await wallet.sendTransaction(transaction, connection);
+
+        // console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`);
+    }
+
+    function createToken() {
+        console.log('Hello')
+        const name = document.getElementById('name').value;
+        const symbol = document.getElementById('symbol').value;
+        const image = document.getElementById('image').value;
+        const initialSupply = document.getElementById('initialSupply').value;
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -12,6 +51,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
